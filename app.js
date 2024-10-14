86f45792c8e742dfe40759391dd7a8c77b0838b7
Abdelrahman <abdelrahmanaymn50@gmail.com>
2024-10-14 20:35:50 +0300

    this is the full code in js,html and css

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-alert("hello")
+// let heart = document.getElementById("heart")
+let hearts = document.querySelectorAll(".image i")
+let header = document.getElementById("header")
+
+
+window.onscroll = function(){
+    if(window.scrollY >= 105){
+        header.style.backgroundColor = "#1b2cc6"
+    } else{
+        header.style.backgroundColor = ""
+    }
+}
+
+hearts.forEach(function(heart) {
+    heart.onclick = function() {
+        if (heart.style.color == "white") {
+            heart.style.color = "red";
+        } else {
+            heart.style.color = "white";
+        }
+    };
+});
